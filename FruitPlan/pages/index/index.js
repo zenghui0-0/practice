cff0f2ec1ec3a0fb30e5f5925b6c1c531ea633ca
january.lee <january.lee@users.noreply.github.com>
2017-06-17 15:24:42 +0800

    小测试

---
+++ FruitPlan/pages/index/index.js
@@ -0,0 +1,38 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    tag:['新品','热卖','特价'],
+    tagDisabled:false,
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  onTag:function(event){
+    if(event.target.id)
+    {
+      console.log(event);
+      this.setData(
+        {
+          tagDisabled:!tagDisabled
+        });
+    }
+  }
+})
