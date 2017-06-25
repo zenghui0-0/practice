2a94049af8b5cd097eea49110cf28cb25a5490dc
zenghui <784515773@qq.com>
2017-06-25 18:35:54 +0800

    zh20170625
    编辑home目录前保存一波

--- FruitPlan/pages/home/home.js
+++ FruitPlan/pages/home/home.js
@@ -1,14 +1,96 @@
 var App = getApp()
 Page({
   data: {
-    latitude: '',
-    longitude: '',
-    district: '',
-    formatted_address: '',
-    motto: 'Hello World',
-    userInfo: {},
-    tag:['新品','热卖','特价'],
-    tagDisabled:[true,false,false],
+    data: {
+      goods: {
+        1: {
+          id: 1,
+          name: '娃娃菜',
+          pic: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/goods/1.jpg',
+          sold: 1014,
+          price: 2
+        },
+        2: {
+          id: 2,
+          name: '金针菇',
+          pic: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/goods/2.jpg',
+          sold: 1029,
+          price: 3
+        },
+        3: {
+          id: 3,
+          name: '方便面',
+          pic: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/goods/2.jpg',
+          sold: 1030,
+          price: 2
+        },
+        4: {
+          id: 4,
+          name: '粉丝',
+          pic: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/goods/2.jpg',
+          sold: 1059,
+          price: 1
+        },
+        5: {
+          id: 5,
+          name: '生菜',
+          pic: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/goods/2.jpg',
+          sold: 1029,
+          price: 2
+        },
+        6: {
+          id: 6,
+          name: '白菜',
+          pic: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/goods/1.jpg',
+          sold: 1064,
+          price: 2
+        },
+        7: {
+          id: 7,
+          name: '杏鲍菇',
+          pic: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/goods/2.jpg',
+          sold: 814,
+          price: 3
+        },
+        8: {
+          id: 8,
+          name: '香菇',
+          pic: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/goods/1.jpg',
+          sold: 124,
+          price: 3
+        },
+        9: {
+          id: 9,
+          name: '猴头菇',
+          pic: 'http://wxapp.im20.com.cn/impublic/waimai/imgs/goods/1.jpg',
+          sold: 102,
+          price: 5
+        }
+      },
+      goodsList: [
+        {
+          id: 'hot',
+          classifyName: '全部',
+          goods: [1, 2, 3, 4, 5]
+        },
+        {
+          id: 'new',
+          classifyName: '水果',
+          goods: [1, 3]
+        },
+        {
+          id: 'vegetable',
+          classifyName: '饮料',
+          goods: [1, 6, 5]
+        }
+      ],
+      cart: {
+        count: 0,
+        total: 0,
+        list: {}
+      },
+      showCartDetail: false
+    },
   },
  
   onLoad(){
