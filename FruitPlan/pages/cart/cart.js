7a354be4a69a005d8a90aa1a72926cea35dd0310
january.lee <january.lee@users.noreply.github.com>
2017-06-27 17:03:46 +0800

    购物车
    购物车

--- FruitPlan/pages/cart/cart.js
+++ FruitPlan/pages/cart/cart.js
@@ -5,7 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    cartData:[
+      {
+        fruitId:1,
+        fruitName:"香蕉",
+        count:3,//加入的数量
+        price:3.5,//单价
+      },
+      {
+        fruitId: 2,
+        fruitName: "苹果",
+        count: 2,//加入的数量
+        price: 5.2,//单价
+      },
+      {
+        fruitId: 3,
+        fruitName: "蜜桃",
+        count: 2,//加入的数量
+        price: 8.62,//单价
+      },
+    ],
   },
 
   /**
@@ -62,5 +81,53 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  onAddCount:function(e) {
+    var cartData = this.data.cartData;
+    const fruitId = e.currentTarget.dataset.fruitid;
+    for(var i = 0;i<cartData.length;i++)
+    {
+      if (fruitId == cartData[i].fruitId)
+      {
+        cartData[i].count++;
+        this.setData({
+          cartData:cartData,
+        });
+      }
+    }
+    
+  },
+
+  onSubCount:function(e) {
+    var cartData = this.data.cartData;
+    var fruitId = e.currentTarget.dataset.fruitid;
+    for (var i = 0; i < cartData.length; i++) {
+      if (fruitId == cartData[i].fruitId) {
+        if (cartData[i].count == 1) {
+          var that = this;
+          var name = cartData[i].fruitName;
+          var index = i;
+          wx.showModal({
+            title: '从购物车中移除',
+            content: '确定将' + name + '从购物车中移除？',
+            success:function(res)
+            {
+              if(res.confirm){
+                cartData.splice(index,1);
+                that.setData({
+                  cartData:cartData,
+                })
+              }
+            }
+          });
+          return;
+        }
+        cartData[i].count--;
+        this.setData({
+          cartData: cartData,
+        });
+      }
+    }
   }
 })
